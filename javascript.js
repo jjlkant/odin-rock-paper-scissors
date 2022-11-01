7899be4c943db3a4ad51206c2addb900d04b7382
Jeroen Kant <jjlkant@gmail.com>
2022-11-01 23:03:26 +0100

    ENH: Develop getComputerChoice for random Rock/Paper/Scissors

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log("Hello World!")
+function getComputerChoice() {
+    let choice;
+    choice = Math.floor(Math.random() * 3);
+    if (choice == 0) {
+        return "Rock"
+    } else {
+        if (choice == 1) {
+            return "Paper"
+        } else {
+            if (choice == 2) {
+                return "Scissors"
+            } 
+        }
+    }
+}
+
